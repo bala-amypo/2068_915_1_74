dd0a1c40968b815fc3a6aec0964d7d36e70f297b
bala-amypo <bala@amypo.in>
2025-12-14 13:43:55 +0000

    User commit

--- src/main/java/com/example/demo/model/Claim.java
+++ src/main/java/com/example/demo/model/Claim.java
@@ -1,5 +1,18 @@
 package com.example.demo.model;
+import java.time.LocalDate;
+import jakarta.persistence.*;
 
+@Entity
 public class Claim {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    private Policy policy;
+
+    private LocalDate claimDate;
+    private Double claimAmount;
+    private String description;
+
+    private String Status="PENDING";
 }
---
+++ src/main/java/com/example/demo/model/FraudRule.java
@@ -0,0 +1,8 @@
+package com.example.demo.model;
+
+public class FraudRule {
+    private Long id;
+    private String rulename;
+    private String conditionField;
+    private 
+}
