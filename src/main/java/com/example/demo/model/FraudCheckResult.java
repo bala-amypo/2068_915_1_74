e5555b607297dd5cc9910aa52e2ea6e4f7e4ea23
bala-amypo <bala@amypo.in>
2025-12-14 13:53:55 +0000

    User commit

--- src/main/java/com/example/demo/model/FraudCheckResult.java
+++ src/main/java/com/example/demo/model/FraudCheckResult.java
@@ -1,6 +1,22 @@
 package com.example.demo.model;
 
-import java
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
 public class FraudCheckResult {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @OneToOne
+    private Claim claim;
+
+    private Boolean isFraudulent;
+    private String triggeredRuleName;
+    private String rejectionReason;
+    private LocalDateTime checkedAt;
+
+    @PrePersist
+    public void onCreate(){
+        checkedAt = LocalDateTime.now();
+    }
 }
