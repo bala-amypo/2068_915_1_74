69a5e45a313a16940ca2942dbe85b31fab2d1d86
bala-amypo <bala@amypo.in>
2025-12-14 12:58:54 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -4,10 +4,21 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAI api(){
-        return new OpenAPI().components(new Components().addSecuritySchemes("bearerAuth", new SecurityScheme().type()))
+    public OpenAPI api(){
+        return new OpenAPI()
+        .components(new Components()
+        .addSecuritySchemes("bearerAuth",
+        new SecurityScheme()
+         .type(SecurityScheme.Type.HTTP)
+         .scheme("bearer")
+         .bearerFormat("JWT"))).addSecurityItem(new SecurityRequirement().addList("bearerAuth"));
     }
 }
