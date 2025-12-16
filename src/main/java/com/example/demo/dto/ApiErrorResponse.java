2fb1aee0e0e97adf52d3c35723925857deb8e5a1
bala-amypo <bala@amypo.in>
2025-12-16 16:13:50 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ApiErrorResponse.java
@@ -0,0 +1,31 @@
+package com.example.demo.dto;
+
+public class ApiErrorResponse {
+
+    private int status;
+    private String message;
+
+    public ApiErrorResponse() {
+    }
+
+    public ApiErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
