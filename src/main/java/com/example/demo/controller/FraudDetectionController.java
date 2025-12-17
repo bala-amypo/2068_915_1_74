4037c29dad695519610dd2cc9f57d12033cf6541
bala-amypo <bala@amypo.in>
2025-12-17 05:03:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/FraudDetectionController.java
+++ src/main/java/com/example/demo/controller/FraudDetectionController.java
@@ -1,5 +1,7 @@
 package.com.example.demo.controller;
 
+import com.example.demo.service.FraudDetectionService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
