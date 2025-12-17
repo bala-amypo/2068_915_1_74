0a59bfc1e31bce5c667a406bbd4c5ad5a4add691
bala-amypo <bala@amypo.in>
2025-12-17 04:53:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/FraudDetectionController.java
+++ src/main/java/com/example/demo/controller/FraudDetectionController.java
@@ -1,3 +1,11 @@
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @RestController
 @RequestMapping("/api/fraud-check")
 public class FraudDetectionController {
